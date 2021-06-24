6aeafe443bd3cd55f5195612fe5309d86f3a1d10
koichimatz <koichi.matsumoto@gmail.com>
2021-06-24 06:18:05 +0000

    [Add]Devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_20_014638) do
+ActiveRecord::Schema.define(version: 2021_06_24_045141) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "opinion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_06_20_014638) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
