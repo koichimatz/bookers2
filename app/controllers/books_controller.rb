e6b3d794b6ffc06ecd8d09432fcfc91487604ade
koichimatz <koichi.matsumoto@gmail.com>
2021-06-25 12:00:52 +0000

    [Add]Signup/SigninFunction git commit -m[Add]Signup/SigninFunction]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,12 @@
 class BooksController < ApplicationController
 
+def new
+  @book = Book.new
+end
 
 def create
   @book = Book.new(book_params)
+  @book.user_id = current_user.id
   if @book.save
   redirect_to book_path(@book.id)
   flash[:notice] = 'Book was successfully created.'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_045141) do
+ActiveRecord::Schema.define(version: 2021_06_24_075821) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "body"
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_06_24_045141) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
