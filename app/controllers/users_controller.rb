5a1c60ed7d20dbf5260e46a12a08cd5105e73856
koichimatz <koichi.matsumoto@gmail.com>
2021-06-26 12:48:30 +0000

    [Add]book index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+  @user = current_user
+  @new_book = Book.new
+  @users = User.all
+  end
+
+  def create
+     @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    flash[:notice] = 'Book was successfully created.'
+    else
+    @books = Book.all
+    render :index
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:image)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  attachment :image
+  attachment :profile_image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
 end
